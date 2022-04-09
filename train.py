d8f09e2ed207b1036e3755fde7ec5cc9ad468941
XihanLiu <92864700+XihanLiu@users.noreply.github.com>
2022-04-09 19:25:14 -0400

    Split dataset into train and test of names with labels.

---
+++ train.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import os
+import random
+dataset_path = r'/Users/liuxihan/Desktop/compressed sensing and sparse recovery/CroppedYale'
+dataset_list_path = r'/Users/liuxihan/Desktop/compressed sensing and sparse recovery/Dictionary-learning-vs-Deep-learning'
+with open(os.path.join(dataset_list_path,'train.txt'),'w') as train:
+	with open(os.path.join(dataset_list_path,'test.txt'),'w') as test:
+		for dataset_dir in  os.listdir(dataset_path):
+			num_test = 0
+			for sample in os.listdir(os.path.join(dataset_path,dataset_dir)):
+				if '.pgm' in sample:
+					flag = random.randint(0,1)
+					if flag == 1 & num_test < 20:
+						test.write(sample+' '+sample[5:7]+'\n')
+						num_test+=1
+	
+					else:
+						train.write(sample+' '+sample[5:7]+'\n')
+train.close()
+test.close()	
